8577e271d38409bf760b970b632844d1f7661767
ewduong <duonge1@wit.edu>
2015-08-18 18:25:26 -0400

    some change of plans

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="css/test.css" />
+<html>
+	<head>
+		<title>Test SQL Stuff</title>
+	</head>
+	<body>
+		<p class="swag">swag</p>
+		<button id="btnCreateDB" onclick="createDB()">Swag Button</button>
+	</body>
+</html>
