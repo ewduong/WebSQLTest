0a6ca3d0e8843058007715b6d9fe84247d0057ac
ewduong <duonge1@wit.edu>
2015-08-19 03:54:35 -0400

    tried to add a bit of flair to some stuff/added part of add user part

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
-<link rel="stylesheet" type="text/css" href="css/test.css" />
+<link rel="stylesheet" type="text/css" href="css/main.css" />
 <html>
 	<head>
 		<title>Test SQL Stuff</title>
 	</head>
 	<body>
-		<form method="post" action="process.php">
-			<label>MySQL Host</label>
-			<input type="text" name="mysql_host" />
-			<br />
-			<label>MySQL Username</label>
-			<input type="text" name="mysql_user" />
-			<br />
-			<label>MySQL Password</label>
-			<input type="text" name="mysql_pass" />
-			<br />
+		<div id="box1">
+			<form method="post" action="process.php">
+				<h1>MySQL Details</h1>
+				<label>MySQL Host</label>
+				<input type="text" placeholder="localhost" name="mysql_host" />
+				<br />
+				<label>MySQL Username</label>
+				<input type="text" placeholder="username" name="mysql_user" />
+				<br />
+				<label>MySQL Password</label>
+				<input type="password" placeholder="password" name="mysql_pass" />
+		</div>
+		<div id="box2">
+			<h1>Add Database</h1>
 			<label>Database Name</label>
-			<input type="text" name="database_name" />
-			<br />
-			<input type="submit" value="Add Database">
-		</form>
+				<input type="text" name="database_name" />
+				<br />
+				<input type="submit" value="Add Database">	
+		</div>
+		<div id="box3">
+			<h1>Register</h1>
+			<label>Username</label>
+				<input type="text" placeholder="username" name="user" />
+				<br />
+				<label>Password</label>
+				<input type="password" placeholder="password" name="pass" />
+				<br /><br />
+				<input type="submit" value ="Add User">
+		</div>
+			</form>
 	</body>
 </html>
